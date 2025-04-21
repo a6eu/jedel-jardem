c657c57902980ba59ab088a4cea7f63ecccf1dfe
Temirlan <temabeu2003@gmail.com>
2025-04-21 22:22:14 +0500

    feat: can upload files

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
 const auth = require('../middlewares/authMiddleware');
-const {array} = require("../multer");
+const upload = require("../multer");
 
-router.post('/', auth, array('files'), messageController.sendMessage);
+router.post('/', auth, upload.array('files'), messageController.sendMessage);
 router.get('/:chatId', auth, messageController.getChatMessages);
 
 module.exports = router;
