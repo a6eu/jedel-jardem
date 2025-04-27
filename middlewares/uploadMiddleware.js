24f2ddd0e6a10f38c8d4612f88151da4b40c195b
Temirlan <temirlan.abeu@dataart.com>
2025-04-28 04:17:12 +0500

    fix: fix for deploy

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,14 +1,13 @@
 const multer = require('multer');
-const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
     },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, uniqueSuffix + path.extname(file.originalname));
-    },
+    }
 });
 
-module.exports = multer({storage});
+const upload = multer({ storage });
